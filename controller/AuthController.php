5304a0b7ec91cf6284b93b861fca7cf6e823d475
Fede Gratti <federicogratti@gmail.com>
2015-10-03 20:56:55 -0300

    check permission on server

--- controller/AuthController.php
+++ controller/AuthController.php
@@ -2,15 +2,15 @@
 
 class AuthController
 {
-    public static function checkPermission($resourceCalled)
+    public static function checkPermission()
     {
 
         echo debug_backtrace()[1]['function'];
         session_start();
 
-        if (!isset($_SESSION['role'])) header('Location: /login');
+        //if (!isset($_SESSION['role'])) header('Location: /login');
 
-        if(!(new AuthModel())->authenticate($_SESSION['role'],$resourceCalled)) header('Location: /login');
+        //if(!(new AuthModel())->authenticate($_SESSION['role'],$resourceCalled)) header('Location: /login');
 
     }
 }
