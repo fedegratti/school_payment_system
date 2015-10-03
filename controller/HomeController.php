2b5a4ce921f7e3c8b366a3642e3f090bdc85fcdd
julietavuan <julietavuan@gmail.com>
2015-10-03 20:25:35 -0300

    bd con las entradas de los metodos

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -4,7 +4,6 @@ class HomeController
 {
     public static function showView()
     {
-        AuthController::checkPermission();
         $view = new HomeView();
         $view->show();
     }
--- grupo_2.sql
+++ grupo_2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 03, 2015 at 11:30 PM
--- Server version: 5.6.26
--- PHP Version: 5.6.12
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-10-2015 a las 01:24:00
+-- Versión del servidor: 5.6.21-log
+-- Versión de PHP: 5.5.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,16 +14,16 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Database: `grupo_2`
+-- Base de datos: `grupo_2`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `auth_mapper`
+-- Estructura de tabla para la tabla `auth_mapper`
 --
 
 CREATE TABLE IF NOT EXISTS `auth_mapper` (
@@ -35,11 +35,11 @@ CREATE TABLE IF NOT EXISTS `auth_mapper` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `configuration`
+-- Estructura de tabla para la tabla `configuration`
 --
 
 CREATE TABLE IF NOT EXISTS `configuration` (
-  `keyValue` int(11) NOT NULL,
+`keyValue` int(11) NOT NULL,
   `numberValue` int(50) DEFAULT NULL,
   `textValue` mediumtext COLLATE utf8_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -47,11 +47,11 @@ CREATE TABLE IF NOT EXISTS `configuration` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `fee`
+-- Estructura de tabla para la tabla `fee`
 --
 
 CREATE TABLE IF NOT EXISTS `fee` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `year` int(11) NOT NULL,
   `month` int(11) NOT NULL,
   `number` int(11) NOT NULL,
@@ -60,16 +60,23 @@ CREATE TABLE IF NOT EXISTS `fee` (
   `collectorPayment` decimal(10,0) NOT NULL,
   `createDate` date DEFAULT NULL,
   `deleted` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `fee`
+--
+
+INSERT INTO `fee` (`id`, `year`, `month`, `number`, `amount`, `kind`, `collectorPayment`, `createDate`, `deleted`) VALUES
+(1, 2, 2015, 2, '2', 0, '2', '2015-10-03', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `guardian`
+-- Estructura de tabla para la tabla `guardian`
 --
 
 CREATE TABLE IF NOT EXISTS `guardian` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `idUser` int(11) NOT NULL,
   `kind` int(1) NOT NULL,
   `lastName` mediumtext COLLATE utf8_unicode_ci NOT NULL,
@@ -80,86 +87,48 @@ CREATE TABLE IF NOT EXISTS `guardian` (
   `phone` int(20) DEFAULT NULL,
   `address` mediumtext COLLATE utf8_unicode_ci,
   `deleted` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `guardian`
+-- Volcado de datos para la tabla `guardian`
 --
 
 INSERT INTO `guardian` (`id`, `idUser`, `kind`, `lastName`, `firstName`, `birthDate`, `sex`, `email`, `phone`, `address`, `deleted`) VALUES
-(1, 1, 1, 'Rodriguez', 'Ah re loca', '2015-10-01', 1, 'juli@asd.com', 1234, '13 e 44 y 56', 0),
-(2, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(3, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(4, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(5, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(6, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(7, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(8, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(9, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(10, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(11, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(12, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(13, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(14, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(15, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(16, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(17, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(18, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(19, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(20, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(21, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(22, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(23, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(24, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(25, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(26, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(27, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(28, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(29, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(30, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(31, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(32, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(33, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(34, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(35, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(36, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(37, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(38, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(39, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(40, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(41, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(42, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(43, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(44, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(45, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(46, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(47, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(48, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(49, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(50, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0),
-(51, 0, 0, 'apellido', 'nombre', NULL, 0, 'asd@asd.com', 1234567, 'direcion re loca', 0);
+(1, 0, 0, 'a', 'a', '0002-02-02', 0, 'ASD@ERT.COM', 2, 'a', 0),
+(2, 0, 0, 'juana', 'juana', '0002-02-02', 0, 'az@hotmail.com', 2, 'a', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `guardian_student_relationship`
+-- Estructura de tabla para la tabla `guardian_student_relationship`
 --
 
 CREATE TABLE IF NOT EXISTS `guardian_student_relationship` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `studentId` int(11) NOT NULL,
   `guardianId` int(11) NOT NULL,
   `deleted` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `guardian_student_relationship`
+--
+
+INSERT INTO `guardian_student_relationship` (`id`, `studentId`, `guardianId`, `deleted`) VALUES
+(1, 0, 0, 0),
+(2, 0, 1, 0),
+(3, 0, 2, 0),
+(4, 0, 1, 0),
+(5, 0, 2, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `payment`
+-- Estructura de tabla para la tabla `payment`
 --
 
 CREATE TABLE IF NOT EXISTS `payment` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `idStudent` int(11) NOT NULL,
   `idFee` int(11) NOT NULL,
   `date` date NOT NULL,
@@ -172,22 +141,50 @@ CREATE TABLE IF NOT EXISTS `payment` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `resource`
+-- Estructura de tabla para la tabla `resource`
 --
 
 CREATE TABLE IF NOT EXISTS `resource` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `description` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `resource`
+--
+
+INSERT INTO `resource` (`id`, `description`) VALUES
+(1, 'showView'),
+(2, 'addFeeView'),
+(3, 'showView'),
+(4, 'addFeeView'),
+(5, 'addFeeAction'),
+(6, 'addGuardianView'),
+(7, 'addGuardianAction'),
+(8, 'listGuardiansView'),
+(9, 'associateGuardianAction'),
+(10, 'addStudentView'),
+(11, 'addStudentAction'),
+(12, 'listStudentsView'),
+(13, 'listStudentsWithNameView'),
+(14, 'updateStudentAction'),
+(15, 'deleteStudentAction'),
+(16, 'addUserView('),
+(17, 'addUserAction'),
+(18, 'listUsersView'),
+(19, 'updateUserView'),
+(20, 'updateUserAction'),
+(21, 'deleteUserView'),
+(22, 'deleteUserAction');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `student`
+-- Estructura de tabla para la tabla `student`
 --
 
 CREATE TABLE IF NOT EXISTS `student` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `documentType` int(2) NOT NULL,
   `documentNumber` int(10) NOT NULL,
   `lastName` text COLLATE utf8_unicode_ci NOT NULL,
@@ -203,7 +200,7 @@ CREATE TABLE IF NOT EXISTS `student` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `student`
+-- Volcado de datos para la tabla `student`
 --
 
 INSERT INTO `student` (`id`, `documentType`, `documentNumber`, `lastName`, `fisrtName`, `birthDate`, `sex`, `email`, `address`, `admissionDate`, `graduationDate`, `createDate`, `deleted`) VALUES
@@ -212,27 +209,27 @@ INSERT INTO `student` (`id`, `documentType`, `documentNumber`, `lastName`, `fisr
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Estructura de tabla para la tabla `user`
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `username` mediumtext COLLATE utf8_unicode_ci NOT NULL,
   `email` text COLLATE utf8_unicode_ci,
   `password` mediumtext COLLATE utf8_unicode_ci NOT NULL,
   `enabled` tinyint(1) NOT NULL,
   `role` int(1) NOT NULL,
   `deleted` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `user`
+-- Volcado de datos para la tabla `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `email`, `password`, `enabled`, `role`, `deleted`) VALUES
-(1, 'HOLIS', 'AQWEQWE@asd.com', 'asd', 1, 1, 0),
+(1, 'HOLIS', 'AQWEQWE@asd.com', 'asd', 1, 1, 1),
 (2, 'hola rama ah re', 'rama@gay', 'asd', 1, 0, 0),
-(3, 'asds', 'asd@asd', 'asd', 1, 0, 0),
+(3, 'asds', NULL, 'f10e2821bbbea527ea02200352313bc059445190', 1, 0, 0),
 (4, 'asdasd', NULL, 'asd', 1, 0, 0),
 (5, 'zxc', NULL, 'zxc', 1, 0, 0),
 (6, 'zxczxcasd', NULL, 'asd', 1, 0, 0),
@@ -241,115 +238,105 @@ INSERT INTO `user` (`id`, `username`, `email`, `password`, `enabled`, `role`, `d
 (9, 'qweqwe', NULL, 'qwe', 1, 0, 0),
 (10, 'asdasdsaasdasdasd', NULL, 'asd', 1, 0, 0),
 (11, 'ffdd', NULL, 'dff', 1, 0, 0),
-(12, 'andaaaa', NULL, 'asdasd', 1, 2, 0);
+(12, 'andaaaa', NULL, 'asdasd', 1, 2, 0),
+(13, 'sd', NULL, 'jh', 1, 0, 0);
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `auth_mapper`
---
-ALTER TABLE `auth_mapper`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `configuration`
+-- Indices de la tabla `configuration`
 --
 ALTER TABLE `configuration`
-  ADD PRIMARY KEY (`keyValue`);
+ ADD PRIMARY KEY (`keyValue`);
 
 --
--- Indexes for table `fee`
+-- Indices de la tabla `fee`
 --
 ALTER TABLE `fee`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `guardian`
+-- Indices de la tabla `guardian`
 --
 ALTER TABLE `guardian`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `guardian_student_relationship`
+-- Indices de la tabla `guardian_student_relationship`
 --
 ALTER TABLE `guardian_student_relationship`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `payment`
+-- Indices de la tabla `payment`
 --
 ALTER TABLE `payment`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `resource`
+-- Indices de la tabla `resource`
 --
 ALTER TABLE `resource`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `student`
+-- Indices de la tabla `student`
 --
 ALTER TABLE `student`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user`
+-- Indices de la tabla `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `auth_mapper`
---
-ALTER TABLE `auth_mapper`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `configuration`
+-- AUTO_INCREMENT de la tabla `configuration`
 --
 ALTER TABLE `configuration`
-  MODIFY `keyValue` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `keyValue` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `fee`
+-- AUTO_INCREMENT de la tabla `fee`
 --
 ALTER TABLE `fee`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT for table `guardian`
+-- AUTO_INCREMENT de la tabla `guardian`
 --
 ALTER TABLE `guardian`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=52;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT for table `guardian_student_relationship`
+-- AUTO_INCREMENT de la tabla `guardian_student_relationship`
 --
 ALTER TABLE `guardian_student_relationship`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
--- AUTO_INCREMENT for table `payment`
+-- AUTO_INCREMENT de la tabla `payment`
 --
 ALTER TABLE `payment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `resource`
+-- AUTO_INCREMENT de la tabla `resource`
 --
 ALTER TABLE `resource`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
 --
--- AUTO_INCREMENT for table `student`
+-- AUTO_INCREMENT de la tabla `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
