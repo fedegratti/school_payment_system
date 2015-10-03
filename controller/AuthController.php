9901960899044e57ff7156bed4c83ff4e0a7760d
Fede Gratti <federicogratti@gmail.com>
2015-10-03 20:10:50 -0300

    probando la magia

--- controller/AuthController.php
+++ controller/AuthController.php
@@ -4,11 +4,13 @@ class AuthController
 {
     public static function checkPermission($resourceCalled)
     {
+
+        echo debug_backtrace()[1]['function'];
         session_start();
 
-        if (!isset($_SESSION['role'])) header('Location: /login');
+        //if (!isset($_SESSION['role'])) header('Location: /login');
 
-        if(!(new AuthModel())->authenticate($_SESSION['role'],$resourceCalled)) header('Location: /login');
+        //if(!(new AuthModel())->authenticate($_SESSION['role'],$resourceCalled)) header('Location: /login');
 
     }
 }
