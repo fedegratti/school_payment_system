6748e30737e1d4155f02bfcd609d575799c6c39b
julietavuan <julietavuan@gmail.com>
2015-10-03 21:00:36 -0300

    Correccion de errores en la autenticacion

--- controller/AuthController.php
+++ controller/AuthController.php
@@ -2,14 +2,13 @@
 
 class AuthController
 {
-    public static function checkPermission($resourceCalled)
+    public static function checkPermission()
     {
-
-        echo debug_backtrace()[1]['function'];
+        $resourceCalled = debug_backtrace()[1]['function'];
         session_start();
 
         if (!isset($_SESSION['role'])) header('Location: /login');
-
+        //(new AuthModel())->authenticate($_SESSION['role'],$resourceCalled);
         if(!(new AuthModel())->authenticate($_SESSION['role'],$resourceCalled)) header('Location: /login');
 
     }
--- model/AuthModel.php
+++ model/AuthModel.php
@@ -5,7 +5,7 @@ class AuthModel extends PDORepository
 	public function authenticate($role,$resource)
 	{
 		$query="SELECT * FROM resource as re
-				INNER JOIN auth_mapper as am ON (re.id = am.roleId)
+				INNER JOIN auth_mapper as am ON (re.id = am.resourceId)
 				INNER JOIN role as ro ON (ro.id = am.roleId)
 				WHERE re.description = ? AND ro.id = ?
 				";
