de84992ff561261cc8315f7f1579fe39ab95fd47
Fede Gratti <federicogratti@gmail.com>
2015-10-05 01:37:50 -0300

    db actualizada, feeForm dinamico

--- grupo_2.sql
+++ grupo_2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 05, 2015 at 03:51 AM
+-- Generation Time: Oct 05, 2015 at 06:28 AM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -46,20 +46,22 @@ INSERT INTO `auth_mapper` (`id`, `roleId`, `resourceId`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `configuration` (
-  `siteEnabled` tinyint(1) NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `description` text NOT NULL,
-  `contactEmail` varchar(50) NOT NULL,
-  `elementsPerList` int(11) NOT NULL,
-  `disabledSiteMessage` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL,
+  `configuration` text NOT NULL,
+  `value` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `configuration`
 --
 
-INSERT INTO `configuration` (`siteEnabled`, `title`, `description`, `contactEmail`, `elementsPerList`, `disabledSiteMessage`) VALUES
-(1, 'Pagina', 'La pagina mas cheta', 'asd@tuvieja.gmail', 20, 'El sitio esta en mantenimiento');
+INSERT INTO `configuration` (`id`, `configuration`, `value`) VALUES
+(2, 'siteEnabled', '1'),
+(3, 'disabledSiteMessage', 'El sitio se encuentra en mantenimiento'),
+(4, 'title', 'AdministrAnexa'),
+(5, 'description', 'Sitio de gestion de alumnos'),
+(6, 'email', 'soporte@graduada.unlp.edu.ar'),
+(7, 'paginationNumber', '12');
 
 -- --------------------------------------------------------
 
@@ -77,7 +79,7 @@ CREATE TABLE IF NOT EXISTS `fee` (
   `collectorPayment` decimal(10,0) NOT NULL,
   `createDate` date DEFAULT NULL,
   `deleted` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `fee`
@@ -89,7 +91,10 @@ INSERT INTO `fee` (`id`, `year`, `month`, `number`, `amount`, `kind`, `collector
 (3, 5555, 2, 1, '-2', 0, '123', '2015-10-04', 1),
 (4, 2, 1111111, 2, '500', 2, '500', '2015-10-04', 1),
 (5, 3333333, 33333333, 3, '33333333', 2, '333333333', '2015-10-04', 0),
-(6, 1, 1, 1, '1', 0, '1', '2015-10-04', 0);
+(6, 1, 1, 1, '1', 0, '1', '2015-10-04', 0),
+(7, 2, 3, 4, '3', 1, '12', '2015-10-05', 0),
+(8, 2, 2, 5, '2', 2, '2', '2015-10-05', 0),
+(9, 1, 2, 4, '123', 1, '12', '2015-10-05', 0);
 
 -- --------------------------------------------------------
 
@@ -353,7 +358,7 @@ INSERT INTO `user` (`id`, `username`, `email`, `password`, `enabled`, `roleId`, 
 (17, '321', NULL, '5f6955d227a320c7f1f6c7da2a6d96a851a8118f', 1, 3, 0),
 (18, 'pepe', 'a@a', '265392dc2782778664cc9d56c8e3cd9956661bb0', 1, 2, 0),
 (19, 'carlos', 'a@a', 'ab5e2bca84933118bbc9d48ffaccce3bac4eeb64', 1, 1, 0),
-(20, '555', 'a@a', '8effee409c625e1a2d8f5033631840e6ce1dcb64', 1, 1, 0);
+(20, '555', 'a@a', '8effee409c625e1a2d8f5033631840e6ce1dcb64', 1, 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -363,6 +368,12 @@ INSERT INTO `user` (`id`, `username`, `email`, `password`, `enabled`, `roleId`, 
 -- Indexes for table `auth_mapper`
 --
 ALTER TABLE `auth_mapper`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `configuration`
+--
+ALTER TABLE `configuration`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -423,10 +434,15 @@ ALTER TABLE `user`
 ALTER TABLE `auth_mapper`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
+-- AUTO_INCREMENT for table `configuration`
+--
+ALTER TABLE `configuration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+--
 -- AUTO_INCREMENT for table `fee`
 --
 ALTER TABLE `fee`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `guardian`
 --
