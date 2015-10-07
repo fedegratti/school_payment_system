be7b496168b02f8e9fcd7082fc1a5b2ade36d18d
Ramiro <fages_fages@hotmail.com>
2015-10-06 21:43:33 -0300

    Corregido los titulos de los listados de alumnos

--- view/student/ListAdmittedStudentsView.php
+++ view/student/ListAdmittedStudentsView.php
@@ -5,6 +5,7 @@ class ListAdmittedStudentsView extends TwigView
     public function show($students = null)
     {
         self::getTwig()->display('student/listStudents.html.twig',array("students" => $students,
-                                                                        "admitted"=>"true"));
+                                                                        "admitted"=>"true",
+                                                                        "title" => "Alumnos con matricula paga"));
     }
 }
